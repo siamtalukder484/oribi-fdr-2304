24d1e9666f7337120844228cba3ee57789961ce3
Shohelrana63 <shohelrana.beg@gmail.com>
2024-02-20 22:43:41 +0600

    pagination

---
+++ src/components/pagintion/Pagination.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
+import dummyProducts from "../dummoyData/dummyProduct"
+import ProductCard from '../product/ProductCard';
+
+
+
+function Items({ currentItems }) {
+  return (
+    <div className='flex flex-wrap gap-x-[46px]'>
+      {currentItems &&
+        currentItems.map((item,index) => (
+          <div className='w-[370px]'>
+            <ProductCard key={index} allinfo={item} productprice={item.productprice} productname={item.productname} productimage={item.productimage} isnew={item.isnew} discount={item.discount} color={item.color} id={item.id}/>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function Pagination({ itemsPerPage }) {
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = dummyProducts.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(dummyProducts.length / itemsPerPage);
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % dummyProducts.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+   <div className='py-20 flex justify-between'>
+       <ReactPaginate
+        breakLabel="...."
+        nextLabel=""
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel=""
+        containerClassName='flex'
+        pageClassName="m-3"
+        pageLinkClassName=' bg-black text-white p-4'
+        renderOnZeroPageCount={null}
+      />
+   
+   <h2>Products from {itemOffset+1} to {endOffset<dummyProducts.length?endOffset:dummyProducts.length} of {dummyProducts.length}</h2>
+   </div>
+    </>
+  );
+}
+export default Pagination
--- src/pages/shop/Index.jsx
+++ src/pages/shop/Index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import BreadCrumb from '../../components/utilities/BreadCrumb'
+import Pagination from '../../components/pagintion/Pagination'
 
 const Shop = () => {
   return (
     <>
-        <h1>aita shop page hobea</h1>
+        <div className='max-w-container mx-auto'>
+          <div className='py-40'>
+            <h2 className='text-[#262626] text-[49px] font-bold pb-3'>Products</h2>
+          <div className='flex gap-x-2'><BreadCrumb/><span className='text-[#767676] text-xs'>Products</span></div>
+          </div>
+          {/* work area start */}
+          <div className='flex'>
+            <div className='w-3/12'>Shop by Category</div>
+            <div className='w-9/12'>
+              <Pagination itemsPerPage={12} />
+
+            </div>
+          </div>
+          {/* work area end */}
+
+        </div>
     </>
   )
 }
